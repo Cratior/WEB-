ef571c0c3a8e9a87d270d6e3fd0bf084348caa76
Cratior <55932656+Cratior@users.noreply.github.com>
2023-03-27 12:51:55 +0100

    fixed sign up button not working

--- script.js
+++ script.js
@@ -1,91 +1,74 @@
 $(document).ready(function() {
-	$("#signIn").click(function() {
-		$(".container").removeClass("right-panel-active");
+	$('#signUp').click(function() {
+		$('.container').addClass("right-panel-active");
 	});
-	
-	$("#signUp").click(function() {
-		$(".container").addClass("right-panel-active");
+
+	$('#signIn').click(function() {
+		$('.container').removeClass("right-panel-active");
+	});
+
+	function login() {
+		var username = $('#username').val();
+		var password = $('#password').val();
+
+		if (username == "" || password == "") {
+			$('#message').html("Please enter a username and password.");
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "login.php",
+			data: {username: username, password: password},
+			success: function(response) {
+				if (response == "success") {
+					$('#message').html("Login successful.");
+					$('#login-form').addClass("valid");
+					$('#login-form').removeClass("invalid");
+				} else {
+					$('#message').html("Invalid username or password.");
+					$('#login-form').addClass("invalid");
+					$('#login-form').removeClass("valid");
+				}
+			}
+		});
+	}
+
+	function signup() {
+		var username = $('#new-username').val();
+		var email = $('#new-email').val();
+		var password = $('#new-password').val();
+
+		if (username == "" || email == "" || password == "") {
+			$('#signup-message').html("Please enter a username, email, and password.");
+			return;
+		}
+
+		$.ajax({
+			type: "POST",
+			url: "signup.php",
+			data: {username: username, email: email, password: password},
+			success: function(response) {
+				if (response == "success") {
+					$('#signup-message').html("Account created successfully.");
+					$('#signup-form').addClass("valid");
+					$('#signup-form').removeClass("invalid");
+				} else {
+					$('#signup-message').html("Error creating account.");
+					$('#signup-form').addClass("invalid");
+					$('#signup-form').removeClass("valid");
+				}
+			}
+		});
+	}
+
+	$('#login-form').submit(function(event) {
+		event.preventDefault();
+		login();
+	});
+
+	$('#signup-form').submit(function(event) {
+		event.preventDefault();
+		signup();
 	});
 });
-
-function login() {
-	var username = $("#username").val();
-	var password = $("#password").val();
-	
-	$("#message").html("Signing in...");
-	
-	$.ajax({
-		url: "https://raw.githubusercontent.com/cratior/login-data/main/data.json",
-		success: function(data) {
-			try {
-				var accounts = JSON.parse(data).accounts;
-				var found = false;
-				for (var i = 0; i < accounts.length; i++) {
-					if (accounts[i].username == username && accounts[i].password == password) {
-						found = true;
-						break;
-					}
-				}
-				if (found) {
-					$("#message").html("Welcome, " + username + "!");
-					$("#login-form").addClass("valid");
-					setTimeout(function() {
-						$("#login-form").removeClass("valid");
-					}, 1000);
-				} else {
-					$("#message").html("Invalid username or password.");
-					$("#login-form").addClass("invalid");
-					setTimeout(function() {
-						$("#login-form").removeClass("invalid");
-					}, 1000);
-				}
-			} catch (e) {
-				console.log("Error parsing JSON data.");
-				$("#message").html("Error signing in.");
-			}
-		},
-		error: function() {
-			console.log("Error loading JSON data.");
-			$("#message").html("Error signing in.");
-		},
-		complete: function() {
-			$("#username").val("");
-			$("#password").val("");
-		}
-	});
-}
-
-function signup() {
-	var newUsername = $("#new-username").val();
-	var newPassword = $("#new-password").val();
-	
-	$("#signup-message").html("Signing up...");
-	
-	$.ajax({
-		url: "https://jsonplaceholder.typicode.com/posts",
-		method: "POST",
-		data: {
-			title: newUsername,
-			body: newPassword,
-			userId: 1
-		},
-		success: function(data) {
-			$("#signup-message").html("Sign up successful!");
-			$("#signup-form").addClass("valid");
-			setTimeout(function() {
-				$("#signup-form").removeClass("valid");
-			}, 1000);
-		},
-		error: function() {
-			$("#signup-message").html("Error signing up.");
-			$("#signup-form").addClass("invalid");
-			setTimeout(function() {
-				$("#signup-form").removeClass("invalid");
-			}, 1000);
-		},
-		complete: function() {
-			$("#new-username").val("");
-			$("#new-password").val("");
-		}
-	});
-}
