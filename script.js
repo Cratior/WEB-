799a9e5ddabce55a2b740eeb1216945f1f746ec4
Cratior <55932656+Cratior@users.noreply.github.com>
2023-03-27 12:15:32 +0100

    added sighn up

--- script.js
+++ script.js
@@ -16,19 +16,47 @@ function login() {
 				}
 				if (found) {
 					$("#message").html("Welcome, " + username + "!");
+					$("#login-container").addClass("active");
 				} else {
 					$("#message").html("Invalid username or password.");
 					$("#login-form").addClass("shake");
 					setTimeout(function() {
 						$("#login-form").removeClass("shake");
-					}, 1000);
+					}, 500);
 				}
 			} catch (e) {
-				$("#message").html("Error loading accounts.");
+				console.log("Error parsing JSON data.");
 			}
 		},
 		error: function() {
-			$("#message").html("Error loading accounts.");
+			console.log("Error loading JSON data.");
+		}
+	});
+}
+
+function showSignup() {
+	$("#login-container").fadeOut(500, function() {
+		$("#signup-container").fadeIn(500);
+	});
+}
+
+function signup() {
+	var newUsername = $("#new-username").val();
+	var newPassword = $("#new-password").val();
+	
+	$.ajax({
+		url: "https://jsonplaceholder.typicode.com/posts",
+		method: "POST",
+		data: {
+			title: newUsername,
+			body: newPassword,
+			userId: 1
+		},
+		success: function(data) {
+			$("#signup-message").html("Sign up successful!");
+		},
+		error: function() {
+			$("#signup-message").html("Error signing up.");
 		}
 	});
 }
